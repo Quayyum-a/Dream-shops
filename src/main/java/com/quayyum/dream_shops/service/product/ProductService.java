a06deb3d94ff674a0e9ee0f4d025aa39fecfd3e7
Quayyum <quayyumariyo@gmail.com>
2025-09-01 15:46:46 +0100

    feat(product): implement ProductService with basic CRUD operations- Add ProductService class implementing IProductService interface- Include methods for adding, retrieving, updating, and deleting products- Implement methods for filtering products by category, brand, and name- Add method for counting products by

---
+++ src/main/java/com/quayyum/dream_shops/service/product/ProductService.java
@@ -0,0 +1,73 @@
+package com.quayyum.dream_shops.service.product;
+
+import com.quayyum.dream_shops.exceptions.ProductNotFoundException;
+import com.quayyum.dream_shops.model.Product;
+import com.quayyum.dream_shops.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService{
+    private ProductRepository productRepository;
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product not found"));
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository
+                .findById(id)
+                .ifPresentOrElse(productRepository::delete,  () -> {
+                    throw new ProductNotFoundException("Product not found");
+                });
+    }
+
+    @Override
+    public Product updateProduct(Product product, Long productId) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
+}
