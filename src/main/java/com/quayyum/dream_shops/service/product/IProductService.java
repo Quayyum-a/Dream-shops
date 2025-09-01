ac73ca575239f4a51b35d62be2565a5035251009
Quayyum <quayyumariyo@gmail.com>
2025-09-01 15:46:09 +0100

    feat(service): add IProductService interface- Define CRUD operations for Product entities- Include methods for filtering products by category, brand, and name- Add method to count products by brand and name

---
+++ src/main/java/com/quayyum/dream_shops/service/product/IProductService.java
@@ -0,0 +1,19 @@
+package com.quayyum.dream_shops.service.product;
+
+import com.quayyum.dream_shops.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(Product product);
+    List<Product> getAllProducts();
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    Product updateProduct(Product product, Long productId);
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+}
