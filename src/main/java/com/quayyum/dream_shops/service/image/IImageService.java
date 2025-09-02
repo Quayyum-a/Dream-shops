169d5e9ef4ee78da8b638bb6cf18b6d69bf3810e
Quayyum <quayyumariyo@gmail.com>
2025-09-02 17:28:20 +0100

    refactor(service): update IImageService interface- Change saveImage method to saveImages, allowing multiple file uploads- Update method signature to accept List<MultipartFile> instead of single MultipartFile- Add import for ImageDto class- Add import for List class

--- src/main/java/com/quayyum/dream_shops/service/image/IImageService.java
+++ src/main/java/com/quayyum/dream_shops/service/image/IImageService.java
@@ -1,12 +1,15 @@
 package com.quayyum.dream_shops.service.image;
 
+import com.quayyum.dream_shops.dto.ImageDto;
 import com.quayyum.dream_shops.model.Image;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IImageService {
     Image getImageById(Long id);
     void deleteImageById(Long id);
-    Image saveImage(MultipartFile file, Long productId);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
     void updateImage(MultipartFile file, Long ImageId);
 
 }
