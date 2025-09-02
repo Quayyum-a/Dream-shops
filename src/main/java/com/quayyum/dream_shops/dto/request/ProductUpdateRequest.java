d764c5ac8e48bd21b4ab8727636295cbc5d66031
Quayyum <quayyumariyo@gmail.com>
2025-09-02 06:48:58 +0100

    feat(dream_shops): add ProductUpdateRequest DTO- Create a new DTO class for updating product information- Include fields for id, name, brand, price, inventory, description, and category- Implement lombok @Data annotation for automated getters, setters, and other boilerplate code

---
+++ src/main/java/com/quayyum/dream_shops/dto/request/ProductUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.quayyum.dream_shops.dto.request;
+
+import com.quayyum.dream_shops.model.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductUpdateRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
+}
