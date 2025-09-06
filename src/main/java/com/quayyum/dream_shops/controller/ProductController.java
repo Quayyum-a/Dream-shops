3f300686f49f74d34ea83d55475b1d336254dedb
Quayyum <quayyumariyo@gmail.com>
2025-09-06 18:36:36 +0100

    feat(controller): implement product CRUD operations- Add methods for getting product by ID, adding a new product, and updating a product- Implement exception handling for resource not found scenarios- Update API endpoints to follow RESTful conventions

--- src/main/java/com/quayyum/dream_shops/controller/ProductController.java
+++ src/main/java/com/quayyum/dream_shops/controller/ProductController.java
@@ -1,16 +1,20 @@
 package com.quayyum.dream_shops.controller;
 
 
+import com.quayyum.dream_shops.exceptions.ResourceNotFoundException;
 import com.quayyum.dream_shops.model.Product;
+import com.quayyum.dream_shops.request.AddProductRequest;
+import com.quayyum.dream_shops.request.ProductUpdateRequest;
 import com.quayyum.dream_shops.response.ApiResponse;
 import com.quayyum.dream_shops.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,7 +28,33 @@ public class ProductController {
         return ResponseEntity.ok(new ApiResponse("All products found!", products));
     }
 
-    public ResponseEntity<ApiResponse> getProductById(Long id){
+    @GetMapping("product/{productId}/product")
+    public ResponseEntity<ApiResponse> getProductById( @PathVariable Long productId){
+        try {
+            Product product = productService.getProductById(productId);
+            return ResponseEntity.ok(new ApiResponse("Product found!", product));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
 
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest product){
+        try {
+            Product theProduct = productService.addProduct(product);
+            return ResponseEntity.ok(new ApiResponse("Product added successful!", theProduct));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/product/{id}/update")
+    public ResponseEntity<ApiResponse> updateProduct(@RequestBody ProductUpdateRequest request, @PathVariable Long id ){
+        try {
+            Product product = productService.updateProduct(request, id);
+            return ResponseEntity.ok(new ApiResponse("Product updated successful!", product));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
     }
 }
