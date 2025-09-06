8beceb5c7192769bf1b0671fbb290578f2a8d66d
Quayyum <quayyumariyo@gmail.com>
2025-09-06 10:43:39 +0100

    feat(controller): add CategoryController with CRUD operations- Implement RESTful endpoints for category management- Handle exceptions for resource not found and already exists scenarios- Use ApiResponse class for unified response structure- Implement methods for getting all categories, adding, updating, deleting, and retrieving categories by ID or name

---
+++ src/main/java/com/quayyum/dream_shops/controller/CategoryController.java
@@ -0,0 +1,81 @@
+package com.quayyum.dream_shops.controller;
+
+import com.quayyum.dream_shops.exceptions.AlreadyExistsException;
+import com.quayyum.dream_shops.exceptions.ResourceNotFoundException;
+import com.quayyum.dream_shops.model.Category;
+import com.quayyum.dream_shops.response.ApiResponse;
+import com.quayyum.dream_shops.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
+public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("All categories found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error: ", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name ){
+        try {
+            Category thecategory = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Category added successfully!", thecategory));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/{id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id){
+        try {
+            Category thecategory = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category Found!", thecategory));
+        } catch (ResourceNotFoundException e) {
+           return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/{name}/category")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name){
+        try {
+            Category thecategory = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Category Found!", thecategory));
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/category/{id}/delete")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id){
+        try {
+             categoryService.deleteCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category deleted!", null));
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category){
+        try {
+            Category updatedCategory = categoryService.updateCategory(category, id);
+            return ResponseEntity.ok(new ApiResponse("Category Updated!", updatedCategory));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
